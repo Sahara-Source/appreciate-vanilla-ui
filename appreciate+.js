7bfc42ada972e21802c54f7c0026bb0ed02567e5
Joseph Jobbings <joseph.jobbings@kingfisher.com>
2023-12-19 23:20:37 +0000

    add fonts

--- appreciate+.js
+++ appreciate+.js
@@ -1,19 +1,13 @@
-var dialog, okButton
-function showPopup() {
-    dialog.showModal()
-}
+var okButton
+
 function okButtonClickEventListener() {
     okButton.addEventListener("click", event => {
-        console.log("click")
         // Without an action, submitting the form via the default GET method causes a page to reload. 
         // so, prevent form submission
         event.preventDefault();
-        dialog.close()
     })
 } 
 window.onload = () => {
-    dialog = document.getElementById("helpDialog")
     okButton = document.querySelector("html body section form button") 
-    showPopup()
     okButtonClickEventListener()
 }
