4f9617850d225857e286ef7af7737ac2e179b254
Joseph Jobbings <joseph.jobbings@kingfisher.com>
2023-12-19 12:01:21 +0000

    initial commit

---
+++ appreciate+.js
@@ -0,0 +1,19 @@
+var dialog, okButton
+function showPopup() {
+    dialog.showModal()
+}
+function okButtonClickEventListener() {
+    okButton.addEventListener("click", event => {
+        console.log("click")
+        // Without an action, submitting the form via the default GET method causes a page to reload. 
+        // so, prevent form submission
+        event.preventDefault();
+        dialog.close()
+    })
+} 
+window.onload = () => {
+    dialog = document.getElementById("helpDialog")
+    okButton = document.querySelector("html body section dialog form button") 
+    showPopup()
+    okButtonClickEventListener()
+}
