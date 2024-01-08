94c9ca927d4b7313fca716e49622dbd158f4d695
Joseph Jobbings <joseph.jobbings@kingfisher.com>
2024-01-08 12:01:41 +0000

    introduce wiremock

--- js/appreciate+.js
+++ js/appreciate+.js
@@ -1,13 +1,32 @@
 var okButton
 
-function okButtonClickEventListener() {
+// function okButtonClickEventListener() {
+//     okButton.addEventListener("click", event => {
+//         // Without an action, submitting the form via the default GET method causes a page to reload. 
+//         // so, prevent form submission
+//         event.preventDefault();
+//     })
+// } 
+
+function signUpButtonClickEventListener() {
     okButton.addEventListener("click", event => {
         // Without an action, submitting the form via the default GET method causes a page to reload. 
         // so, prevent form submission
+        console.log("Signup button clicked")
         event.preventDefault();
     })
 } 
+
+
+
 window.onload = () => {
-    okButton = document.querySelector("html body section form button") 
-    okButtonClickEventListener()
+    okButton = document.querySelector(".footer-div-signup-button") 
+    signUpButtonClickEventListener()
+
+    // if input is empty: show error
+    // else 
+    //   randomly show "user name taken"
+    // else
+    //    navigate to signed in page
 }
+
